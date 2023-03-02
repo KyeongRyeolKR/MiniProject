8d15fe2ba596df9155e65744e49203687f24229a
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-03-02 09:34:30 +0900

    5단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        List<Post> posts = new ArrayList<>();
+        String command = "";
+        int index = 1;
+        System.out.println("== 명언 앱 ==");
+        while(true) {
+            System.out.print("명령) ");
+            command = sc.nextLine();
+            if(command.equals("종료")) break;
+            if(command.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+                posts.add(new Post(index, author, content));
+                System.out.printf("%d번 명언이 등록되었습니다.\n", index);
+                index++;
+            }
+            if(command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for(int i=posts.size()-1; i>=0; i--) {
+                    System.out.printf("%d / %s / %s\n", posts.get(i).getId(), posts.get(i).getAuthor(), posts.get(i).getContent());
+                }
+            }
+        }
     }
 }
--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -1,2 +1,37 @@
-package org.example;public class Post {
+package org.example;
+
+public class Post {
+    private int id;
+    private String author;
+    private String content;
+
+    public Post(int id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
